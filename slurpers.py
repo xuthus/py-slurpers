7b8aa73ebddba14a2ea7ac7b01d9065cbf4a5010
Sergey Yanzin <xuthus@yandex.ru>
2020-01-03 22:16:14 +0400

    #2 - unit tests

--- slurpers.py
+++ slurpers.py
@@ -1,4 +1,4 @@
-import xml.etree.ElementTree as ET
+from xml.etree import ElementTree
 import _io
 import re
 
@@ -8,6 +8,13 @@ class Constants:
     STRIP_CAPITALIZE = 3
     IGNORE_TAGS = 4
     USE_NAME_FUNCTION = 5
+
+
+def strip_namespace(s: str):
+    p = s.find('}')
+    if p > 0:
+        return s[p+1:]
+    return s
 
 
 def strip_illegal_chars_capitalize(s: str):
@@ -77,21 +84,19 @@ class XmlSlurper:
     def create(cls, data=None, file_name: str = None, illegal_chars: int = Constants.REPLACE_WITH_UNDERSCORES, name_func = None):
         options = {'illegal_chars': illegal_chars, 'name_func': name_func}
         if file_name is not None:
-            return XmlSlurper(cls._get_map(ET.parse(file_name).getroot(), options))
+            return XmlSlurper(cls._get_map(ElementTree.parse(file_name).getroot(), options))
         if isinstance(data, str):
-            return XmlSlurper(cls._get_map(ET.fromstring(data), options))
+            return XmlSlurper(cls._get_map(ElementTree.fromstring(data), options))
         if isinstance(data, _io._TextIOBase):
-            return XmlSlurper(cls._get_map(ET.fromstring(data.read()), options))
-        if isinstance(data, ET.Element):
+            return XmlSlurper(cls._get_map(ElementTree.fromstring(data.read()), options))
+        if isinstance(data, ElementTree.Element):
             return XmlSlurper(cls._get_map(data, options))
         raise TypeError('Illegal input argument [data]')
 
     @classmethod
     def _extract_name(cls, elem, _illegal_chars, name_func):
         result = elem.tag
-        p = result.find('}')
-        if p > 0:
-            result = result[p+1:]
+        result = strip_namespace(result)
         if ('-' in result) or ('.' in result):
             if _illegal_chars == Constants.REPLACE_WITH_UNDERSCORES:
                 result = replace_illegal_chars_with(result, '_')
@@ -136,131 +141,6 @@ class XmlSlurper:
             return result
 
 
-tree = ET.parse('testdata/beatles.xml')
-root = tree.getroot()
-xml = XmlSlurper.create(root)
 
-for m in xml.man:
-    print(m)
-
-xml = XmlSlurper.create(file_name = 'testdata/beatles.xml')
-for beatle in xml.man:
-    print(beatle.surname)
-    #print('{} {} born at {} in {}'.format(' '.join(beatle.name) if isinstance(beatle.name, list) else beatle.name, beatle.surname, beatle.born.year, beatle.born.place))
-
-
-xml = XmlSlurper.create("<data><name>Sergey</name></data>")
-print(xml.name)
-
-xml = XmlSlurper.create("<name>Singletag</name>")
-print(xml)
-
-xml = XmlSlurper.create("<root><man><name>John</name><surname>Lennon</surname><surname>Smith</surname></man><man><name>Mark</name><surname>Twain</surname></man><born><place>Russia</place><year>2018</year></born></root>")
-print(xml.born.place)
-
-
-with open('testdata/singletag.xml', 'r') as f:
-    xml = XmlSlurper.create(f)
-    print(xml)
-
-with open('testdata/test1.xml', 'r') as f:
-    xml = XmlSlurper.create(f)
-    print(xml.born.place)
-
-xml = XmlSlurper.create(file_name='testdata/test1.xml')
-print(xml.born.place)
-
-try:
-    xml = XmlSlurper.create("<name>Singletag</name")
-    print('Failed')
-except:
-    print('OK: Wrong xml handled')
+if __name__ == "__main__":
     pass
-
-
-xml = XmlSlurper.create("<name>Singletag</name>")
-try:
-    print(xml.Name)
-    print('Failed')
-except:
-    print('OK: Wrong tag name handled')
-    pass
-
-
-xml = XmlSlurper.create("<name>Singletag</name>")
-try:
-    print(xml.name[0])
-    print('Failed')
-except:
-    print('OK: Wrong tag index handled')
-    pass
-
-# # test files
-xml = XmlSlurper.create(file_name='testdata/logback.xml')
-print(xml.appender[0].encoder.pattern)
-print(xml.appender[0].filter.level)
-print(len(xml.appender))
-
-xml = XmlSlurper.create(file_name='testdata/test.xml')
-print(xml)
-print(xml.movie[0])
-print(len(xml.movie))
-for movie in xml.movie:
-    print(movie)
-    #print(movie, movie.namespace)
-
-xml = XmlSlurper.create("<name>Singletag</name>")
-print(xml)
-
-xml = XmlSlurper.create(file_name = 'testdata/beatles.xml')
-for man in xml.man:
-    print('{} {} born at {} in {}'.format(' '.join(man.name), man.surname, man.born.year, man.born.place))
-
-xml = XmlSlurper.create(file_name = 'testdata/tags-illegals.xml')
-print(xml.tag_one)
-print(xml.tag_two)
-
-xml = XmlSlurper.create(file_name = 'testdata/tags-illegals.xml', illegal_chars = Constants.STRIP)
-print(xml.tagone)
-print(xml.tagtwo)
-
-xml = XmlSlurper.create(file_name = 'testdata/tags-illegals.xml', illegal_chars = Constants.IGNORE_TAGS)
-try:
-    print(xml.tag_one)
-    print('failed IGNORE_TAGS')
-except:
-    print('ok IGNORE_TAGS')
-    pass
-
-xml = XmlSlurper.create(file_name = 'testdata/tags-illegals.xml', illegal_chars = Constants.REPLACE_WITH_UNDERSCORES)
-print(xml.tag_one)
-print(xml.tag_two)
-
-print(strip_illegal_chars_capitalize('test-tag'))
-print(strip_illegal_chars_capitalize('test-t.ag'))
-print(strip_illegal_chars_capitalize('test-.-tag'))
-print(strip_illegal_chars_capitalize('testtag'))
-print(strip_illegal_chars_capitalize('_'))
-
-xml = XmlSlurper.create(file_name = 'testdata/tags-illegals.xml', illegal_chars = Constants.STRIP_CAPITALIZE)
-print(xml.tagOne)
-print(xml.tagTwo)
-
-xml = XmlSlurper.create(file_name = 'testdata/tags-illegals.xml', illegal_chars = Constants.USE_NAME_FUNCTION, name_func = lambda x: strip_illegal_chars(x) + '__')
-print(xml.tagone__)
-print(xml.tagtwo__)
-
-
-xml = XmlSlurper.create(file_name = 'testdata/cdata.xml')
-print(xml.tag1)
-print(xml.tag2)
-try:
-    print(xml.tag3)
-    print('failed comment')
-except:
-    print('ok comment')
-
-
-xml = XmlSlurper.create(file_name = 'testdata/attributes.xml')
-for country in xml.country:
-    print("{}: {}".format(country.name, country.population))
---
+++ tests.py
@@ -0,0 +1,143 @@
+import unittest
+from slurpers import *
+from xml.etree.ElementTree import ParseError
+
+
+class TestSlurperFunctions(unittest.TestCase):
+    def test_strip_illegal_chars_capitalize(self):
+        self.assertEqual('testone', strip_illegal_chars_capitalize('testone'))
+        self.assertEqual('testone', strip_illegal_chars_capitalize('testone-'))
+        self.assertEqual('testOne', strip_illegal_chars_capitalize('test-one'))
+        self.assertEqual('testOne', strip_illegal_chars_capitalize('test.one'))
+        self.assertEqual('testOne', strip_illegal_chars_capitalize('test-.-one'))
+        self.assertEqual('testOne', strip_illegal_chars_capitalize('test..--one'))
+        self.assertEqual('testOne', strip_illegal_chars_capitalize('test-.-one-.-'))
+
+    def test_replace_illegal_chars_with(self):
+        self.assertEqual('testone', replace_illegal_chars_with('testone', '/'))
+        self.assertEqual('test/one', replace_illegal_chars_with('test-one', '/'))
+        self.assertEqual('test/one', replace_illegal_chars_with('test.one', '/'))
+        self.assertEqual('test///one', replace_illegal_chars_with('test-.-one', '/'))
+        self.assertEqual('test////one', replace_illegal_chars_with('test..--one', '/'))
+        self.assertEqual('test///one///', replace_illegal_chars_with('test-.-one-.-', '/'))
+
+    def test_strip_illegal_chars(self):
+        self.assertEqual('testone', strip_illegal_chars('testone'))
+        self.assertEqual('testone', strip_illegal_chars('test-one'))
+        self.assertEqual('testone', strip_illegal_chars('test.one'))
+        self.assertEqual('testone', strip_illegal_chars('test-.-one'))
+        self.assertEqual('testone', strip_illegal_chars('test--..one'))
+
+    def test_strip_namespace(self):
+        self.assertEqual('test', strip_namespace('test'))
+        self.assertEqual('test', strip_namespace('{http://localhost:8080}test'))
+        self.assertEqual('', strip_namespace('{http://localhost:8080}'))
+
+
+class TestXmlSlurper(unittest.TestCase):
+    def test_Beatles(self):
+        tree = ElementTree.parse('testdata/beatles.xml')
+        root = tree.getroot()
+        xml = XmlSlurper.create(root)
+
+        self.assertEqual(4, len(xml.man))
+
+        xml = XmlSlurper.create(file_name = 'testdata/beatles.xml')
+        for beatle in xml.man:
+            self.assertTrue(beatle.surname in ['Lennon', 'McCartney', 'Starr', 'Harrison'])
+    
+        xml = XmlSlurper.create(file_name = 'testdata/beatles.xml')
+        res = []
+        for man in xml.man:
+            res.append('{} {} born at {} in {}'.format(' '.join(man.name), man.surname, man.born.year, man.born.place))
+        self.assertEqual(
+            'John Winston Lennon born at 1940 in Liverpool\nJames Paul McCartney born at 1942 in Liverpool\nR i n g o Starr born at 1940 in Liverpool\nG e o r g e Harrison born at 1943 in Liverpool',
+            '\n'.join(res)
+        )
+
+    def test_simple(self):
+        xml = XmlSlurper.create("<data><name>Sergey</name></data>")
+        self.assertEqual('Sergey', xml.name)
+
+    def test_single_tag(self):
+        xml = XmlSlurper.create("<name>Singletag</name>")
+        self.assertEqual('Singletag', str(xml))
+
+    def test_second_level(self):
+        xml = XmlSlurper.create("<root><man><name>John</name><surname>Lennon</surname><surname>Smith</surname></man><man><name>Mark</name><surname>Twain</surname></man><born><place>Russia</place><year>2018</year></born></root>")
+        self.assertEqual('Russia', xml.born.place)
+        self.assertEqual('2018', xml.born.year)
+
+    def test_wrong_xml(self):
+        with self.assertRaises(ParseError):
+            xml = XmlSlurper.create("<name>John</name")
+        with self.assertRaises(ParseError):
+            xml = XmlSlurper.create("<name>John</Name>")
+        with self.assertRaises(ParseError):
+            xml = XmlSlurper.create("<name ame>John</name ame>")
+
+    def test_wrong_tag(self):
+        xml = XmlSlurper.create("<root><name>John</name></root>")
+        with self.assertRaises(KeyError):
+            print(xml.Name)
+
+    def test_wrong_index(self):
+        xml = XmlSlurper.create("<root><man><name>John</name></man></root>")
+        with self.assertRaises(KeyError):
+            print(xml.man[0])
+        xml = XmlSlurper.create("<root><man><name>John</name></man><man><name>Paul</name></man></root>")
+        self.assertEqual('John', xml.man[0].name)
+        self.assertEqual('Paul', xml.man[1].name)
+        with self.assertRaises(IndexError):
+            print(xml.man[2])
+
+    def test_logback_xml(self):
+        xml = XmlSlurper.create(file_name='testdata/logback.xml')
+        self.assertEqual('%d{HH:mm:ss} %-5level: %msg%n', xml.appender[0].encoder.pattern)
+        self.assertEqual('INFO', xml.appender[0].filter.level)
+        self.assertEqual('STDOUT', xml.appender[0].name)
+        self.assertEqual(2, len(xml.appender))
+
+    def test_illegals(self):
+        xml = XmlSlurper.create(file_name = 'testdata/tags-illegals.xml')
+        self.assertEqual('value 1', xml.tag_one)
+        self.assertEqual('value 2', xml.tag_two)
+
+        xml = XmlSlurper.create(file_name = 'testdata/tags-illegals.xml', illegal_chars = Constants.STRIP)
+        self.assertEqual('value 1', xml.tagone)
+        self.assertEqual('value 2', xml.tagtwo)
+
+        xml = XmlSlurper.create(file_name = 'testdata/tags-illegals.xml', illegal_chars = Constants.REPLACE_WITH_UNDERSCORES)
+        self.assertEqual('value 1', xml.tag_one)
+        self.assertEqual('value 2', xml.tag_two)
+
+        xml = XmlSlurper.create(file_name = 'testdata/tags-illegals.xml', illegal_chars = Constants.STRIP_CAPITALIZE)
+        self.assertEqual('value 1', xml.tagOne)
+        self.assertEqual('value 2', xml.tagTwo)
+
+        xml = XmlSlurper.create(file_name = 'testdata/tags-illegals.xml', illegal_chars = Constants.USE_NAME_FUNCTION, name_func = lambda x: strip_illegal_chars(x) + '__')
+        self.assertEqual('value 1', xml.tagone__)
+        self.assertEqual('value 2', xml.tagtwo__)
+
+        xml = XmlSlurper.create(file_name = 'testdata/tags-illegals.xml', illegal_chars = Constants.IGNORE_TAGS)
+        with self.assertRaises(KeyError):
+            print(xml.tag_one)
+
+        xml = XmlSlurper.create(file_name = 'testdata/cdata.xml')
+        self.assertEqual('text value', xml.tag1)
+        self.assertEqual('text <message> with CDATA </message>', xml.tag2)
+        with self.assertRaises(KeyError):
+            print(xml.tag3)
+
+    def test_attributes(self):
+        xml = XmlSlurper.create(file_name = 'testdata/attributes.xml')
+        res = []
+        for country in xml.country:
+            res.append("{}: {}".format(country.name, country.population))
+        self.assertEqual(
+            'Russia: 100\nSpain: 30',
+            '\n'.join(res)
+        )
+
+if __name__ == "__main__":
+    unittest.main()
