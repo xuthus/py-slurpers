c3cd9ebdd6964f60f9ac84de95f4d5cf0dc2fbc6
Sergey Yanzin <xuthus@yandex.ru>
2020-01-04 09:21:57 +0400

    refactorings

--- slurpers.py
+++ slurpers.py
@@ -1,6 +1,9 @@
 from xml.etree import ElementTree
 import _io
 import re
+import json
+from abc import ABCMeta, abstractmethod
+
 
 class Constants:
     STRIP = 1
@@ -37,8 +40,7 @@ def strip_illegal_chars(s: str):
     return replace_illegal_chars_with(s, '')
 
 
-class XmlSlurper:
-
+class AbstractSlurper(metaclass=ABCMeta):
     def __init__(self, value):
         self._value = value
         self._illegal_chars = Constants.REPLACE_WITH_UNDERSCORES
@@ -80,37 +82,63 @@ class XmlSlurper:
     def __str__(self):
         return str(self._value)
 
-    @classmethod
-    def create(cls, data=None, file_name: str = None, illegal_chars: int = Constants.REPLACE_WITH_UNDERSCORES, name_func = None):
-        options = {'illegal_chars': illegal_chars, 'name_func': name_func}
-        if file_name is not None:
-            return XmlSlurper(cls._get_map(ElementTree.parse(file_name).getroot(), options))
-        if isinstance(data, str):
-            return XmlSlurper(cls._get_map(ElementTree.fromstring(data), options))
-        if isinstance(data, _io._TextIOBase):
-            return XmlSlurper(cls._get_map(ElementTree.fromstring(data.read()), options))
-        if isinstance(data, ElementTree.Element):
-            return XmlSlurper(cls._get_map(data, options))
-        raise TypeError('Illegal input argument [data]')
 
-    @classmethod
-    def _extract_name(cls, elem, _illegal_chars, name_func):
-        result = elem.tag
-        result = strip_namespace(result)
+class AbstractSlurperBuilder(metaclass=ABCMeta):
+
+    def __init__(self, data=None, file_name: str = None, options: dict = None):
+        self.data = data
+        self.file_name = file_name
+        self.options = options
+
+    @abstractmethod
+    def fromFile(self):
+        return None
+
+    @abstractmethod
+    def fromString(self):
+        return None
+
+    @abstractmethod
+    def fromStream(self):
+        return None
+
+    @abstractmethod
+    def _get_map(self, elem, options: dict):
+        pass
+
+    def _extract_name(self, childName: str, illegal_chars, name_func):
+        result = childName
         if ('-' in result) or ('.' in result):
-            if _illegal_chars == Constants.REPLACE_WITH_UNDERSCORES:
+            if illegal_chars == Constants.REPLACE_WITH_UNDERSCORES:
                 result = replace_illegal_chars_with(result, '_')
-            elif _illegal_chars == Constants.STRIP_CAPITALIZE:
+            elif illegal_chars == Constants.STRIP_CAPITALIZE:
                 result = strip_illegal_chars_capitalize(result)
-            elif _illegal_chars == Constants.STRIP:
+            elif illegal_chars == Constants.STRIP:
                 result = strip_illegal_chars(result)
-            elif _illegal_chars == Constants.IGNORE_TAGS:
+            elif illegal_chars == Constants.IGNORE_TAGS:
                 result = None
         result = result if name_func is None else name_func(result)
         return result
 
-    @classmethod
-    def _get_map(cls, elem, options: dict):
+
+class XmlSlurperBuilder(AbstractSlurperBuilder):
+
+    def fromFile(self):
+        return self._fromTree(ElementTree.parse(self.file_name).getroot())
+
+    def fromString(self):
+        return self._fromTree(ElementTree.fromstring(self.data))
+
+    def fromStream(self):
+        return self._fromTree(ElementTree.fromstring(self.data.read()))
+
+    def fromTree(self):
+        return self._fromTree(self.data)
+
+    def _fromTree(self, tree):
+        return XmlSlurper(self._get_map(tree, self.options))
+
+    def _get_map(self, elem, options: dict):
         if len(elem) == 0:
             return elem.text
         else:
@@ -118,9 +146,9 @@ class XmlSlurper:
             illegal_chars = options['illegal_chars']
             name_func = options['name_func']
             for child in elem:
-                child_name = cls._extract_name(child, illegal_chars, name_func)
+                child_name = self._extract_name(strip_namespace(child.tag), illegal_chars, name_func)
                 if not (child_name is None):
-                    child_map = cls._get_map(child, options)
+                    child_map = self._get_map(child, options)
                     if child_name in result:
                         if isinstance(result[child_name], list):
                             result[child_name].append(child_map)
@@ -140,6 +168,58 @@ class XmlSlurper:
                     result[child_name] = child_map
             return result
 
+
+class JsonSlurperBuilder(AbstractSlurperBuilder):
+
+    def fromFile(self):
+        return None
+
+    def fromString(self):
+        return None
+
+    def fromStream(self):
+        return None
+
+    def _get_map(self, elem, options: dict):
+        return None
+
+
+class XmlSlurper(AbstractSlurper):
+
+    @classmethod
+    def create(cls, data=None, file_name: str = None, illegal_chars: int = Constants.REPLACE_WITH_UNDERSCORES, name_func = None):
+        options = {
+            'illegal_chars': illegal_chars, 
+            'name_func': name_func
+        }
+        builder = XmlSlurperBuilder(data, file_name, options)
+        if file_name is not None:
+            return builder.fromFile()
+        if isinstance(data, str):
+            return builder.fromString()
+        if isinstance(data, _io._TextIOBase):
+            return builder.fromStream()
+        if isinstance(data, ElementTree.Element):
+            return builder.fromTree()
+        raise TypeError('Illegal input argument [data]')
+
+
+class JsonSlurper(AbstractSlurper):
+
+    @classmethod
+    def create(cls, data=None, file_name: str = None, illegal_chars: int = Constants.REPLACE_WITH_UNDERSCORES, name_func = None):
+        options = {
+            'illegal_chars': illegal_chars, 
+            'name_func': name_func
+        }
+        builder = JsonSlurperBuilder(data, file_name, options)
+        if file_name is not None:
+            return builder.fromFile()
+        if isinstance(data, str):
+            return builder.fromString()
+        if isinstance(data, _io._TextIOBase):
+            return builder.fromStream()
+        raise TypeError('Illegal input argument [data]')
 
 
 if __name__ == "__main__":
